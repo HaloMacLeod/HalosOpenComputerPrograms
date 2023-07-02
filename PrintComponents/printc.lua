a6d0e0b43c5a930c8523893daf95b87ece622ea8
HaloMacLeod <44145157+HaloMacLeod@users.noreply.github.com>
2023-07-02 15:26:55 -0700

    printc update

--- PrintComponents/printc.lua
+++ PrintComponents/printc.lua
@@ -6,17 +6,16 @@ if #arg ~= 1 or next(opt) then
    os.exit(1)
 end
 
-local function print_component(arg[1])
-    if arg == "all" then
-        for k, v in pairs(component.list()) do
+if arg == "all" then
+    for k, v in pairs(component.list()) do
+        print(tostring(k)..": "..tostring(v))
+    end
+else
+    for k, v in pairs(component.list()) do
+        if string.match(string.lower(v), string.lower(arg[1])) then
             print(tostring(k)..": "..tostring(v))
-        end
-    else
-        for k, v in pairs(component.list()) do
-            if string.match(string.lower(v), string.lower(arg[1])) then
-                print(tostring(k)..": "..tostring(v))
-            end
         end
     end
 end
+
 
