cb8c8ecb053e8d1b3284557966767296ea2de726
HaloMacLeod <44145157+HaloMacLeod@users.noreply.github.com>
2023-07-02 15:22:41 -0700

    printc update

--- PrintComponents/printc.lua
+++ PrintComponents/printc.lua
@@ -1,4 +1,24 @@
-local version = "1"
-for k, v in pairs(component.list()) do
-  print(tostring(k)..": "..tostring(v))
+local shell = require "shell"
+
+local arg,opt = shell.parse(...)
+if #arg ~= 1 or next(opt) then
+   print("Use: print component list or specific types.")
+   os.exit(1)
 end
+
+local function print_component(arg[1])
+    if arg == "all" then
+        for k, v in pairs(component.list()) do
+            print(tostring(k)..": "..tostring(v))
+          end
+        else
+            for k, v in pairs(component.list()) do
+                if string.match(string.lower(v), string.lower(arg[1])) then
+                    print(tostring(k)..": "..tostring(v))
+                end
+              end
+            end
+        end
+    end
+end
+
