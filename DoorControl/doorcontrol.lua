30468a1280a5f3e24819efd0031df65c2d5139e0
HaloMacLeod <44145157+HaloMacLeod@users.noreply.github.com>
2023-07-02 19:42:53 -0700

    update doorcontrol

--- DoorControl/doorcontrol.lua
+++ DoorControl/doorcontrol.lua
@@ -31,8 +31,8 @@ local settings = {
     doorTime = 5,
     maingpuaddr = "3627db68-bb13-4df9-ba22-dca33b75c1a8",
     secgpuaddr = "cb6d9713-0569-437a-91a9-415abbcbad8f",
-    maingpu = component.get(maingpuaddr),
-    secgpu = component.get(secgpuaddr),
+    maingpu = component.get(settings.maingpuaddr),
+    secgpu = component.get(settings.secgpuaddr),
     mainScreen = "735f9afb-653f-48ed-bad6-d2dcd8ab4e17",
     secScreen = "305099b1-c556-491b-b6e6-fec508f75d58",   
 }
