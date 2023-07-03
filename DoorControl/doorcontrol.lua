4528754cf7450b7dcc4acb061eb74e4ee66fe466
HaloMacLeod <44145157+HaloMacLeod@users.noreply.github.com>
2023-07-02 20:21:33 -0700

    doorcontrol eol update

--- DoorControl/doorcontrol.lua
+++ DoorControl/doorcontrol.lua
@@ -97,10 +97,11 @@ end
 -- If loading one doesn't work, exit with an error.
 if settings.whitelist then
   print("Loading whitelist...")
-local function isAllowed(player)
-    for k, v in pairs(settings.whitelist) do
-        if string.lower(v) == string.lower(player) then
-            return true
+    local function isAllowed(player)
+        for k, v in pairs(settings.whitelist) do
+            if string.lower(v) == string.lower(player) then
+                return true
+            end
         end
     end
 end
