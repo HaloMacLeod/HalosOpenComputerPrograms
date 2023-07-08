0277e57f56103cb9bb143bf24c2cb98350f11074
HaloMacLeod <44145157+HaloMacLeod@users.noreply.github.com>
2023-07-07 22:55:32 -0700

    update at1

--- AutoTinkersOne/at1.lua
+++ AutoTinkersOne/at1.lua
@@ -9,6 +9,7 @@ local ser = require "serialization"
 
 local smeltIron = component.proxy("9882b341-aa1d-40c7-8971-147d0ad60c66")
 local smeltCopper = component.proxy("582498d8-3387-4a9f-b588-e56570252d93")
+local smeltLead = component.proxy("a1a71aa3-1207-4792-a919-aebca4537ce2")
 local Drain = component.proxy("d7ad0645-174b-4344-8fde-e37ab3088548")
 
 local arg,opt = shell.parse(...)
@@ -19,8 +20,9 @@ end
 
 if string.lower(arg[1]) == "iron" then
     if arg[3] ~= nil then
-        smeltIron.setOutput(sides.west, 15)
         smeltCopper.setOutput(sides.west, 0)
+        smeltLead.setOutput(sides.west, 0)
+        smeltIron.setOutput(sides.west, 15)
 
         wait(arg[3])
         smeltIron.setOutput(sides.west,0)
@@ -28,12 +30,15 @@ if string.lower(arg[1]) == "iron" then
         os.exit(1)
     end
     if arg[2] ~= nil and string.lower(arg[2]) == "drain"
-        Drain.setOutput(sides.top, 15)
+        smeltLead.setOutput(sides.west, 0)
         smeltCopper.setOutput(sides.west, 0)
+        Drain.setOutput(sides.top, 15)
         smeltIron.setOutput(sides.west, 15)
         os.exit(1)
     end
     Drain.setOutput(sides.top, 0)
+    smeltLead.setOutput(sides.west, 0)
+    smeltCopper.setOutput(sides.west, 0)
     smeltIron.setOutput(sides.west, 15)
     os.exit(1)
 elseif string.lower(arg[1]) == "copper" then
