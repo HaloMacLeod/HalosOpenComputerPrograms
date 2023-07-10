6a345d82d3e8ed1cd4a118fd1b6fbb45182fad10
HaloMacLeod <44145157+HaloMacLeod@users.noreply.github.com>
2023-07-09 17:22:31 -0700

    changed printc

--- PrintComponents/printc.lua
+++ PrintComponents/printc.lua
@@ -13,10 +13,32 @@ if #arg ~= 1 or next(opt) then
    os.exit(1)
 end
 
+Comps = {}
+
 if string.lower(arg[1]) == "all" then
-    for k, v in pairs(component.list()) do
-        print(tostring(k)..": "..tostring(v))
+    if arg[2] ~= nil and string.lower(arg[2]) == "save" then
+        for k, v in pairs(component.list()) do
+            print(tostring(k)..": "..tostring(v))
+            table.insert(Comps, "comp: "..k.." = "..v)
+        end
+        file=io.open("/comps","w")
+        file:write(serial.serialize(Comps))
+        file:close()
+    else
+        for k, v in pairs(component.list()) do
+            print(tostring(k)..": "..tostring(v))
+        end
     end
+elseif arg[3] ~= nil and string.lower(arg[3]) == "save" then
+    for k, v in pairs(component.list()) do
+        if string.match(string.lower(v), string.lower(arg[1])) then
+            print(tostring(k)..": "..tostring(v))
+            table.insert(Comps, "comp: "..k.." = "..v)
+        end
+    end
+    file=io.open("/comps","w")
+    file:write(serial.serialize(Comps))
+    file:close()
 else
     for k, v in pairs(component.list()) do
         if string.match(string.lower(v), string.lower(arg[1])) then
