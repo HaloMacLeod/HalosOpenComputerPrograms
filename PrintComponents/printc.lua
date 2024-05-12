46cfd30d4484cb327b7a75a48fb5335aa90115f3
HaloMacLeod <44145157+HaloMacLeod@users.noreply.github.com>
2024-05-12 16:08:23 -0700

    updated save function

--- PrintComponents/printc.lua
+++ PrintComponents/printc.lua
@@ -8,7 +8,7 @@ local internet = require "internet"
 local ser = require "serialization"
 
 local arg,opt = shell.parse(...)
-if #arg ~= 1 or next(opt) then
+if #arg < 1 or #arg > 2 or next(opt) then
    print("Use: print component list or specific types.")
    os.exit(1)
 end
@@ -16,35 +16,25 @@ end
 Comps = {}
 
 if string.lower(arg[1]) == "all" then
-    if arg[2] ~= nil and string.lower(arg[2]) == "save" then
-        for k, v in pairs(component.list()) do
-            print(tostring(k)..": "..tostring(v))
-            table.insert(Comps, "comp: "..k.." = "..v)
-        end
-        file=io.open("/comps","w")
-        file:write(serial.serialize(Comps))
-        file:close()
-    else
-        for k, v in pairs(component.list()) do
-            print(tostring(k)..": "..tostring(v))
-        end
-    end
-elseif arg[3] ~= nil and string.lower(arg[3]) == "save" then
     for k, v in pairs(component.list()) do
-        if string.match(string.lower(v), string.lower(arg[1])) then
-            print(tostring(k)..": "..tostring(v))
+        print(tostring(k)..": "..tostring(v))
+        if arg[2] ~= nil and string.lower(arg[2]) == "save" then
             table.insert(Comps, "comp: "..k.." = "..v)
         end
     end
-    file=io.open("/comps","w")
-    file:write(serial.serialize(Comps))
-    file:close()
 else
     for k, v in pairs(component.list()) do
         if string.match(string.lower(v), string.lower(arg[1])) then
             print(tostring(k)..": "..tostring(v))
+            if arg[2] ~= nil and string.lower(arg[2]) == "save" then
+                table.insert(Comps, "comp: "..k.." = "..v)
+            end
         end
     end
 end
 
-
+if #Comps > 0 then
+    file=io.open("/comps","w")
+    file:write(ser.serialize(Comps))
+    file:close()
+end
